59609506b76b6d6b1e9364b1fab760eca319d3b8
Travis Martin <tjmartin2003@gmail.com>
2020-11-02 11:15:09 -0600

    De-grossified help command and minor fix to settings lib.

--- cmd/help.js
+++ cmd/help.js
@@ -10,70 +10,49 @@ module.exports = {
 	category: cmd.categories.UNCATEGORIZED,
 	execute: (request) => {
 		const categories = {};
-		const commands = [];
 		for (const command of request.message.client.commands) {
 			if (!(command.category in categories)) { categories[command.category] = []; }
 			categories[command.category].push(command);
 		}
 
+		// Assign command/category to print.
+		let queryCategoryName, queryCommand;
 		if (request.args.length) {
-			const command = request.message.client.commands.find((command) => command.aliases.includes(request.args[0]));
-			if (command) {
-				// Command-specific output.
-				return log.discord(request.message.channel, {
-					fields: [
-						{ name: 'Aliases', value: `${command.aliases}` },
-						{ name: 'Usage', value: command.usage },
-						{ name: 'Category', value: command.category }
-					],
-					description: command.description,
-					title: `Command help: ${command.aliases[0]}`
-				});
-			} else {
-				const categoryName = Object.getOwnPropertyNames(categories).find((categoryName) => categoryName == request.args[0]);
-				if (categoryName) {
-					// Category-specific output.
-					let output = {
-						fields: [],
-						description: `Category "${categoryName}" commands:`,
-						title: `Category help: ${categoryName}`
-					};
-					for (const command of categories[categoryName]) {
-						output.fields.push({ name: command.aliases[0], value: command.description });
-					}
-					return log.discord(request.message.channel, output);
-				} else {
-					// Unknown help query.
-					return log.discord(request.message.channel, {
-						color: log.colors.WARNING,
-						description: `Unknown command or category "${request.args[0]}".`,
-						title: 'Help'
-					});
-				}
-			}
-		} else {
-			// Argument-less output.
-			let description = `Also view more commands by searching categories: \`${cmd.categories.UNCATEGORIZED}\``;
+			queryCommand = request.message.client.commands.find((command) => command.aliases.includes(request.args[0]));
+			if (!queryCommand) { queryCategoryName = Object.getOwnPropertyNames(categories).find((categoryName) => categoryName == request.args[0]); }
+		}
+		if (!queryCategoryName && !queryCommand) { queryCategoryName = cmd.categories.UNCATEGORIZED; }
+
+		if (queryCommand) {
+			return log.discord(request.message.channel, {
+				fields: [
+					{ name: 'Aliases', value: `${queryCommand.aliases}`, inline: false },
+					{ name: 'Usage', value: queryCommand.usage, inline: false },
+					{ name: 'Category', value: queryCommand.category, inline: false }
+				],
+				description: queryCommand.description,
+				title: `Command: ${queryCommand.aliases[0]}`
+			});
+		} else if (queryCategoryName) {
+			const output = {
+				fields: [],
+				description: `This is the help for a category (\`${queryCategoryName}\`). Other commands can be found in the other categories: \`${cmd.categories.UNCATEGORIZED}\``,
+				title: `Category: ${queryCategoryName}`
+			};
 			for (const categoryName in categories) {
 				if (
 						categoryName == cmd.categories.UNCATEGORIZED ||
 						categoryName == cmd.categories.HIDDEN ||
 						categoryName == cmd.categories.OWNER
 				) { continue; }
-				description += `, \`${categoryName}\``;
+				output.description += `, \`${categoryName}\``;
 			}
-			description += '.\nUncategorized commands:';
-
-			const categoryName = cmd.categories.UNCATEGORIZED;
-			let output = {
-				fields: [],
-				description: description,
-				title: `Category help: ${categoryName}`
-			};
-			for (const command of categories[categoryName]) {
-				output.fields.push({ name: command.aliases[0], value: command.description });
-			}
-			return log.discord(request.message.channel, output);
+			output.description += `.\n\n\`${queryCategoryName}\` commands:`;
+			for (const command of (categories[queryCategoryName])) { output.fields.push({ name: command.aliases[0], value: command.description }); }
+			return log.discord(request.message.channel, output); // Success.
+		} else {
+			log.unified(request.message.channel, 'The help command tried to return an improper value.');
+			return false; // Failure.
 		}
 	}
 };
